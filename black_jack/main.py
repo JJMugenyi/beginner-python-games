fbe032820f1bb2ff79ab26959330c61c9a319ad5
Jordan J. Mugenyi <jordanjamesxvi@outlook.com>
2023-05-31 13:07:21 +0100

    Updated Efficiency
    Added proper input validation and error handling in the deposit() and get_bet() functions to ensure valid inputs. Simplified the condition checks in the deposit() and get_bet() functions to reduce unnecessary code. Replaced the use of input() for quitting the game with exit() for better control flow.

--- black_jack/main.py
+++ black_jack/main.py
@@ -4,66 +4,42 @@ from sys import platform
 
 # Clears command line
 def clear():
-    if platform == "linux" or platform == "linux2":
-        # linux
-        os.system("clear")
-    elif platform == "darwin":
-        # OS X
-        os.system("clear")
-    elif platform == "win32":
-        # Windows...
-        os.system("CLS")
+    os.system("clear" if os.name == "posix" else "cls")
 
-# Global Val
-n_decks = 6    #standard casino's number
+# Global Variables
+n_decks = 6    # Standard casino's number
 suit = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 def new_cards():
     total = suit * 4 * n_decks
-
     return total
-
-balance = 0
-bet = 0
 
 # Gets player's deposit
 def deposit():
     while True:
-        balance = input("How much would you like to deposit?  $")
-        if balance.isdigit():
-            balance = int(balance)
-            if balance > 0:
-                break
-            else:
-                print("You can't deposit $0")
+        balance = input("How much would you like to deposit? $")
+        if balance.isdigit() and int(balance) > 0:
+            return int(balance)
         else:
-            print(f"{balance} is not a number")
-    return balance
+            print("Invalid deposit amount. Please enter a positive number.")
 
 # Gets player's bet amount
-def get_bet():
+def get_bet(balance):
     while True:
-        amount = input("How much would you like to bet?  $")
-        if amount.isdigit():
-            amount = int(amount)
-            if amount > balance:
-                print(f"You cannot bet more than your current balance (${balance})")
-            elif amount > 0:
-                break
-            else:
-                print("You can't bet $0")
+        amount = input(f"How much would you like to bet? (Available balance: ${balance}) $")
+        if amount.isdigit() and 0 < int(amount) <= balance:
+            return int(amount)
         else:
-            print(f"{amount} is not a number")
-    return amount
+            print("Invalid bet amount. Please enter a valid bet.")
 
 # Draws card from the cards
-def deal_card():
+def deal_card(cards):
     card = random.choice(cards)
     cards.remove(card)
     return card
 
 # Calculates total hand
-def calcScore(hand):
+def calc_score(hand):
     if sum(hand) == 21 and len(hand) == 2:
         return 0
     if 11 in hand and sum(hand) > 21:
@@ -72,74 +48,66 @@ def calcScore(hand):
     return sum(hand)
 
 # Checks for winner
-def checkWinner(user_score, computer_score):
+def check_winner(user_score, computer_score, balance, bet):
     if user_score > 21 and computer_score > 21:
-        print("    You lose, you went over. 😤")
+        print("You lose, you went over. 😤")
         return balance - bet
-    
     if user_score == computer_score:
-        print("    Draw 🙃")
+        print("Draw 🙃")
         return balance
     elif computer_score == 0:
-        print("    You lose, dealer has Blackjack 😱")
+        print("You lose, dealer has Blackjack 😱")
         return balance - bet
     elif user_score == 0:
-        print("    You Win with a Blackjack 😎")
+        print("You Win with a Blackjack 😎")
         return balance + int(bet * 1.5)
     elif user_score > 21:
-        print("    You lose, you went over 😭")
+        print("You lose, you went over 😭")
         return balance - bet
     elif computer_score > 21:
-        print("    You win, dealer went over 😁")
+        print("You win, dealer went over 😁")
         return balance + bet
     elif user_score > computer_score:
-        print("    You win 😃")
+        print("You win 😃")
         return balance + bet
     else:
-        print("    You lose 😤")
+        print("You lose 😤")
         return balance - bet
 
-def play_game():
-    global balance   
-    global cards
+def play_game(balance, cards):
     user_cards = []
     computer_cards = []
     is_game_over = False
+
     if not cards:
         cards = new_cards()
 
-
-
-
     for _ in range(2):
-        user_cards.append(deal_card())
-        computer_cards.append(deal_card())
+        user_cards.append(deal_card(cards))
+        computer_cards.append(deal_card(cards))
     
-  
     while not is_game_over:
-        user_score = calcScore(user_cards)
-        computer_score = calcScore(computer_cards)
-        print(f"\n   Your cards: {user_cards}, current score: {user_score}")
-        print(f"   Dealer's card: [{computer_cards[0]}, _ ]")
+        user_score = calc_score(user_cards)
+        computer_score = calc_score(computer_cards)
+        print(f"\nYour cards: {user_cards}, current score: {user_score}")
+        print(f"Dealer's card: [{computer_cards[0]}, _ ]")
     
         if user_score == 0 or computer_score == 0 or user_score > 21:
             is_game_over = True
         else:
             user_should_deal = input("\nType 'y' to get another card, type 'n' to pass: ")
-            if user_should_deal == "y":
-              user_cards.append(deal_card())
+            if user_should_deal.lower() == "y":
+                user_cards.append(deal_card(cards))
             else:
-              is_game_over = True
+                is_game_over = True
     
     while computer_score != 0 and computer_score < 17:
-        computer_cards.append(deal_card())
-        computer_score = calcScore(computer_cards)
+        computer_cards.append(deal_card(cards))
+        computer_score = calc_score(computer_cards)
     
-    print(f"\n    Your final hand: {user_cards}, final score: {user_score}")
-    print(f"    Computer's final hand: {computer_cards}, final score: {computer_score}")
-    balance = checkWinner(user_score, computer_score)
-    
-
+    print(f"\nYour final hand: {user_cards}, final score: {user_score}")
+    print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
+    return check_winner(user_score, computer_score, balance, bet)
 
 clear()
 balance = deposit()
@@ -151,14 +119,13 @@ while True:
         quit()
     play = input("Press enter to play ('q' to quit)")
     clear()
-    if play =="q":
+    if play.lower() == "q":
         exit()
-    balance = balance
+    print(f"Your current balance is ${balance}\n")
+    bet = get_bet(balance)
 
     if len(cards) < 300:
         print("Time to shuffle the cards!")
         cards = new_cards()
 
-    print(f"Your current balance is ${balance}\n")
-    bet = get_bet()
-    play_game()
+    balance = play_game(balance, cards)
